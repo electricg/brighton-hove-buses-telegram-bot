fcfc3012c2f13df37b14092ddc7d73aa91387bbb
Giulia Alfonsi <electric.g@gmail.com>
2018-02-07 00:20:20 -0800

    A little bit more modern javascript

--- test/lib/config.test.js
+++ test/lib/config.test.js
@@ -2,15 +2,14 @@ const config = require('../../lib/config');
 
 describe('Config', () => {
   it('should return a valid setting', done => {
-    var c = config.get('telegramToken');
-    (typeof c).should.equal('string');
+    const output = config.get('telegramToken');
+    (typeof output).should.equal('string');
     done();
   });
 
   it('should return undefined for an invalid setting', done => {
-    var c = config.get('xxx');
-    console.log(c);
-    (typeof c).should.equal('undefined');
+    const output = config.get('xxx');
+    (typeof output).should.equal('undefined');
     done();
   });
 });
